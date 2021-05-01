27c36bd7da2dcaf5ee1e69c9f4c9dc10d9f8e263
Rafael de Abreu Ferreira <rafael.abreu.ferreira10@gmail.com>
2021-05-01 01:40:57 -0300

    criada a função para buscar a api (falta criar uma função para mostrar a listagem na página)

---
+++ js/main.js
@@ -0,0 +1,128 @@
+
+/* elementos do formulário*/
+const $btnForm = document.querySelector('#btnform');
+const $errorFormNome =document.querySelector('#errorFormNome');
+const $errorFormCpf =document.querySelector('#errorFormCpf');
+const $errorFriend = document.querySelector('#errorFriend');
+const $errorGenero = document.querySelector('#errorFormGenero');
+const $inputName = document.querySelector('#input1');
+const $inputCpf = document.querySelector('#input3');
+const $inputFriend = document.querySelector('#input4');
+const $btnShare = document.querySelector('#btnShare');
+const $masculino = document.querySelector('#masculino');
+const $feminino = document.querySelector('#feminino')
+
+
+/* variáveis globais e listiners */
+$btnForm.addEventListener('click', validarForm);
+$inputName.addEventListener('keyup', validarInputName);
+$inputCpf.addEventListener('keyup', validarInputCpf);
+$inputFriend.addEventListener('keyup', validarInputFriend);
+
+
+/*chamando a api dos produtos */
+const productsList = () =>{
+    const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1"
+
+    fetch(url)
+        .then(response => response.json())
+        .then(products => {
+            console.log(products)
+        })
+}
+
+productsList()
+
+
+
+/* validar campo nome do formulário */
+function validarInputName(){
+    const inputName = $inputName.value;
+    if (inputName === ''){
+        $errorFormNome.style.display = 'block';
+        $inputName.style.border = '1px solid red';
+        $inputName.style.borderRadius = '1px';
+        $inputName.style.marginBottom = '0px';
+        return false;
+    } else{
+        $errorFormNome.style.display = 'none';
+        $inputName.style.borderRadius = '1px';
+        $inputName.style.border = '1px solid #979797';
+        $inputName.style.marginBottom = '34px';
+        return true;
+    }
+
+}
+
+
+/* validar campo cpf do formulário */
+function validarInputCpf(){
+    const inputCpf = $inputCpf.value;
+    if (inputCpf === ''){
+        $errorFormCpf.style.display = 'block';
+        $inputCpf.style.border = '1px solid red';
+        $inputName.style.borderRadius = '1px';
+        $inputCpf.style.marginBottom = '0px';
+        return false;
+    } else{
+        $errorFormCpf.style.display = 'none';
+        $inputCpf.style.border = '1px solid #979797';
+        $inputName.style.borderRadius = '1px';
+        $inputCpf.style.marginBottom = '34px';
+        return true;
+    }
+}
+
+
+/* validar formulário de cadastro */
+function validarForm(){
+    validarInputName()
+    validarInputCpf()
+
+    if(validarInputName() == ""){
+        return false
+    } 
+    if(validarInputCpf() == ""){
+        return false
+    }if(!$masculino.checked && !$feminino.checked){
+        $errorGenero.style.display = 'block'
+
+    }if($masculino.checked){
+        $errorGenero.style.display = 'none'
+        !$feminino.value.checked	
+    }
+    else if($feminino.checked){
+        $errorGenero.style.display = 'none'
+        !$masculino.value.checked	
+    }
+}
+
+
+/* validar nome do amigo do formulário */
+function validarInputFriend(){
+    const inputFriend = $inputFriend.value;
+    if (inputFriend === ''){
+        $errorFriend.style.display = 'block';
+        $errorFriend.style.textAlign = 'left';
+        $inputFriend.style.border = '1px solid red';
+        $inputName.style.borderRadius = '1px';
+        $inputFriend.style.marginBottom = '0px';
+
+        return false;
+    } else{
+        $errorFriend.style.display = 'none';
+        $inputFriend.style.border = '1px solid #979797';
+        $inputName.style.borderRadius = '1px';
+        $inputFriend.style.marginBottom = '34px';
+        return true;
+    }
+}
+
+/* validar formulário de compatilhamento */
+
+function validarFormShare(){
+    validarInputFriend()
+    if(validarInputFriend() == ""){
+        return false
+    } 
+}
